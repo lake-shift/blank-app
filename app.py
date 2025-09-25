3b7813298d7220a96e5930257a6f92686028e46f
lake-shift <support@lakeshift.in>
2025-09-26 01:59:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
     <style>
         .sticky-button {
             position: fixed;
-            top: 30px;
+            top: 50px;
             left: 20px;
             z-index: 9999;
         }

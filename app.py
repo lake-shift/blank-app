2e21e2798e47d6941f88435fe62f6a04abbd9491
lake-shift <support@lakeshift.in>
2025-09-26 02:55:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown(
-    "<h4 style='text-align:center; color:gray;'>Migrate SAP HANA Calculation Views to Databricks in Seconds 🚀</h4>",
+    "<h4 style='text-align:center; color:gray;'>Migrate SAP HANA Calculation Views to Databricks in one click 🚀</h4>",
     unsafe_allow_html=True
 )
 

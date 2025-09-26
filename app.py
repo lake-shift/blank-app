174e4c95b6589bccf8e82e2b94b4a751242f492d
lake-shift <support@lakeshift.in>
2025-09-26 06:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ job_id = st.secrets["JOB_ID"]
 sender_email = st.secrets["SE"]
 receiver_email = st.secrets["RE"]
 api_key = st.secrets["AK"]
-
-# --- Authentication ---
-USERNAME = st.secrets("USERNAME")
-PASSWORD = st.secrets("PASSWORD")
+api1 = st.secrets("api1")
+api2 = st.secrets("api2")
 
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
@@ -43,13 +41,13 @@ def login():
     )
 
     st.write("### 🔐 Login Required")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    api11 = st.text_input("username")
+    api22 = st.text_input("Password", type="password")
 
     col1, col2 = st.columns(2)
     with col1:
         if st.button("Login"):
-            if lower(username) == USERNAME and password == PASSWORD:
+            if lower(api1) == api11 and api2 == api22:
                 st.session_state.authenticated = True
                 st.success("Login successful ✅")
                 st.rerun()
